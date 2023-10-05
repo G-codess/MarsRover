c6d11383c77916ce5ed52d77ca709dbcd48d47ab
Gowtham <codedimensions@protonmail.com>
2023-10-05 07:38:02 +0530

    Add some more functionality

--- rover.py
+++ rover.py
@@ -1,19 +1,16 @@
-# Import the logging module to handle logging information
 import logging
 
 # Define cardinal directions, movement rules, and instruction mapping
 orient = ['N', 'E', 'S', 'W']
 move = {'N': (0, 1), 'E': (1, 0), 'S': (0, -1), 'W': (-1, 0)}
-instructions = {'L': 'tleft', 'R': 'tright', 'M': 'move'}
+instructions = {'L': 'turn_left', 'R': 'turn_right', 'M': 'move_forward'}
 
 # Configure logging settings
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Define the Rover class
 class Rover:
     def __init__(self, x, y, xmax, ymax, bearing, obstacles):
-        # Initialize Rover with initial position, grid boundaries, direction, and obstacles
         self.x = x
         self.y = y
         self.xmax = xmax
@@ -21,61 +18,54 @@ class Rover:
         self.bearing = bearing
         self.obstacles = set(obstacles)
 
-    def tright(self):
-        # Turn the Rover to the right
-        self.bearing = orient[(orient.index(self.bearing) + 1) % len(orient)]
+    def move(self, dx, dy, action):
+        new_position = (self.x + dx, self.y + dy)
 
-    def tleft(self):
-        # Turn the Rover to the left
-        self.bearing = orient[(orient.index(self.bearing) - 1) % len(orient)]
-
-    def move(self):
-        # Move the Rover based on its current direction
-        xmod = self.x + move[self.bearing][0]
-        ymod = self.y + move[self.bearing][1]
-        new_position = (xmod, ymod)
-
-        # Check for obstacles and boundaries before moving
         if new_position not in self.obstacles:
-            if 0 <= xmod <= self.xmax and 0 <= ymod <= self.ymax:
-                self.x = xmod
-                self.y = ymod
+            if 0 <= new_position[0] <= self.xmax and 0 <= new_position[1] <= self.ymax:
+                self.x, self.y = new_position
                 logging.info(f'Rover moved to ({self.x}, {self.y}) facing {self.bearing}')
             else:
                 raise ValueError('Out of bounds, please try again!!')
         else:
-            raise ValueError(f'Obstacle detected at {new_position}. Rover cannot move!')
+            raise ValueError(f'Obstacle detected at {new_position}. Rover cannot {action}!')
 
-# Main program execution
+    def display_status(self):
+        obstacle_status = "No obstacles detected." if not any(
+            (self.x + dx, self.y + dy) in self.obstacles for dx, dy in move.values()) else "Obstacle detected."
+        print(f"Rover is at ({self.x}, {self.y}) facing {self.bearing}. {obstacle_status}")
+
+class SimpleRover(Rover):
+    def turn_right(self):
+        self.bearing = orient[(orient.index(self.bearing) + 1) % len(orient)]
+
+    def turn_left(self):
+        self.bearing = orient[(orient.index(self.bearing) - 1) % len(orient)]
+
+    def move_forward(self):
+        self.move(move[self.bearing][0], move[self.bearing][1], "move")
+
 if __name__ == '__main__':
     try:
-        # User input for grid size and obstacles
         xmax, ymax = map(int, input('Enter grid size (xmax ymax): ').split())
         obstacles = []
 
-        # User input for obstacle positions
         obstacle_count = int(input('Enter the number of obstacles: '))
         for _ in range(obstacle_count):
             obstacle = tuple(map(int, input('Enter obstacle position (x y): ').split()))
             obstacles.append(obstacle)
 
-        # User input for starting position and direction
         x, y, bearing = input('Enter starting coordinates and direction (x y bearing): ').split()
-        rover = Rover(int(x), int(y), xmax, ymax, bearing, obstacles)
+        rover = SimpleRover(int(x), int(y), xmax, ymax, bearing, obstacles)
 
-        # User input for movement commands
-        commands = input('Enter commands (M for move, L for turn left, R for turn right): ')
-        for i in commands:
-            if i not in 'MRL':
-                raise ValueError(f'Invalid instruction "{i}": use M or R or L - please try again')
+        commands = input('Enter commands (M for move forward, L for turn left, R for turn right): ')
+        for command in commands:
+            if command not in 'MRL':
+                raise ValueError(f'Invalid instruction "{command}": use M or R or L - please try again')
             else:
-                getattr(rover, instructions[i])()
+                getattr(rover, instructions[command])()
 
-        # Display final position and status report
-        print(f"Final Position: ({rover.x}, {rover.y}, {rover.bearing})")
-
-        obstacle_status = "No obstacles detected." if not any((rover.x + dx, rover.y + dy) in rover.obstacles for dx, dy in move.values()) else "Obstacle detected."
-        print(f"Status Report: \"Rover is at ({rover.x}, {rover.y}) facing {rover.bearing}. {obstacle_status}\"")
+        rover.display_status()
 
     except ValueError as e:
-        logger.error(f'Error: {e}')
+        logging.error(f'Error: {e}')
